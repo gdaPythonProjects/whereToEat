36bf7ccc492911f7696e148c08662f9353e9f30d
Daniel Modzelewski <modzel132@gmail.com>
2018-11-26 23:53:46 +0100

    Małe poprawy

--- lokalizacja.py
+++ lokalizacja.py
@@ -1,16 +1,19 @@
 import requests
 
-Key ="b1216d51234f336579b6aed812325d24"
-#funkcja która pobiera dane z API
-def funkcja_szer_dlug(szerokosc,dlugosc,zasieg):
-    listaMiejsce=[]
-    listaAdresow=[]
+Key = "b1216d51234f336579b6aed812325d24"
+Key1="TFpFrRxkN4mshvr9bKNn0JkGwl9Fp1Mir8UjsnIpACinjydvjm"
 
-    url1 = "https://developers.zomato.com/api/v2.1/search?count=100&lat="+str(dlugosc)+"&lon="+str(szerokosc)+"&sort=real_distance"
-    r = requests.get(url1, headers={'user-key': Key})
-    data =r.json()
+# funkcja która pobiera dane z API
+def funkcja_szer_dlug(szerokosc, dlugosc, zasieg):
+    listaMiejsce = []
+    listaAdresow = []
+
+    url = "https://developers.zomato.com/api/v2.1/search?lat=" + str(dlugosc) + "&lon=" + str(
+        szerokosc) + "&sort=real_distance"
+    r = requests.get(url, headers={'user-key': Key})
+    data = r.json()
     miejsca = data['restaurants']
-    #Wejście do JSON'A
+    # Wejście do JSON'A
     for miejsce in miejsca:
         listaAdresow.append((miejsce['restaurant']['location']['latitude']))
         listaAdresow.append((miejsce['restaurant']['location']['longitude']))
@@ -18,5 +21,12 @@ def funkcja_szer_dlug(szerokosc,dlugosc,zasieg):
         listaAdresow.append((miejsce['restaurant']['user_rating']['aggregate_rating']))
         listaAdresow.append((miejsce['restaurant']['menu_url']))
 
-    #poukladane1 = "<br />".join(listaAdresow)
+    # poukladane1 = "<br />".join(listaAdresow)
     return listaAdresow
+def funkcja_miasto():
+    listaMiejsce = []
+    url = "https://hoppit.p.mashape.com/getPlaces"
+    r = requests.get(url, headers={"X-Mashape-Key": Key1})
+    data = r.json()
+
+    print(str(data))
--- polaczenie.py
+++ polaczenie.py
@@ -1,25 +1,29 @@
-from flask import Flask,render_template,request
-from lokalizacja import funkcja_szer_dlug
+from flask import Flask, render_template, request
+from lokalizacja import funkcja_szer_dlug,funkcja_miasto
 import webbrowser
-#postawienie sewera
-app = Flask(__name__,template_folder='Strona')
 
-@app.route('/wynik',methods=['POST'])
+# postawienie sewera
+app = Flask(__name__, template_folder='Strona')
+
+
+@app.route('/wynik', methods=['POST'])
 def wynik():
+
     szerokosc = request.form['szerokosc']
     dlugosc = request.form['dlugosc']
     zasieg = request.form['zasieg']
-    #x jest potrzebny do skrócenia nazwy funkcji
-    x = funkcja_szer_dlug(szerokosc,dlugosc,zasieg)
-    #wysłanie danych do wynik.html
-    return render_template("wynik.html",przeslij_html=x)
-
+    # x jest potrzebny do skrócenia nazwy funkcji
+    x = funkcja_szer_dlug(szerokosc, dlugosc, zasieg)
+    # wysłanie danych do wynik.html
+    if szerokosc == '':
+        return zasieg
+    else:
+        return render_template("wynik.html", przeslij_html=x)
 @app.route('/')
 def index():
     return render_template("index.html")
 
+
 if __name__ == '__main__':
     webbrowser.open_new("http://127.0.0.1:5000")
     app.run(debug=False)
-
-
