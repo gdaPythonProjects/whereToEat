bce9585511328038fe38268e511d0523a0fed938
Daniel Modzelewski <modzel132@gmail.com>
2018-11-26 19:48:20 +0100

    b

--- lokalizacja.py
+++ lokalizacja.py
@@ -1,12 +1,12 @@
 import requests
 
-Key ="0d1356a0c37774b2f1b132913381d9b1"
+Key ="b1216d51234f336579b6aed812325d24"
 #funkcja która pobiera dane z API
 def funkcja_szer_dlug(szerokosc,dlugosc,zasieg):
     listaMiejsce=[]
     listaAdresow=[]
 
-    url1 = "https://developers.zomato.com/api/v2.1/search?count=100&lat="+str(dlugosc)+"&lon="+str(szerokosc)+"&radius="+zasieg
+    url1 = "https://developers.zomato.com/api/v2.1/search?count=100&lat="+str(dlugosc)+"&lon="+str(szerokosc)+"&radius="+zasieg+"&sort=real_distance"
     r = requests.get(url1, headers={'user-key': Key})
     data =r.json()
     miejsca = data['restaurants']
