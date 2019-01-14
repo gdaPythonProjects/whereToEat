56bd153471ad3dfe185a760ecb04151ced918cd9
Daniel Modzelewski <daniel.modzelewski5@gmail.com>
2019-01-14 21:16:02 +0100

    Update Walidator.py

--- Walidator.py
+++ Walidator.py
@@ -1,7 +1,11 @@
 import re
 def dzialanie_na_stringu(dane):
-    x= re.compile('[a-zA-Zńąęćółśźż]+')
+    x = re.compile('[a-zA-Zńąęćółśźż]+')
     dane = x.findall(dane)
     print(str(''.join(dane)))
 def dzialanie_na_liczbie(liczba):
-	x=re.sub(r',','.',liczba)
+    liczba = liczba.replace(',', '.')
+    x=re.compile(r"[+-]?\d{1,2}\.?\d*")
+    liczba = x.findall(liczba)
+    print(liczba)
+dzialanie_na_liczbie("dd5g-e,.,.,.,.,.,.,.#@3.213d")
