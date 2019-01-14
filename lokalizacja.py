c87afcf9cedb6ace2351bf6b3524ea3b3fcfb700
Daniel Modzelewski <daniel.modzelewski5@gmail.com>
2019-01-14 19:45:51 +0100

    Dodanie funkcjonalności wyszukiwania miast

--- lokalizacja.py
+++ lokalizacja.py
@@ -5,7 +5,7 @@ Key = "0d1356a0c37774b2f1b132913381d9b1"
 url = "https://developers.zomato.com/api/v2.1/"
 
 
-# funkcja która pobiera dane z API
+# funkcja która pobiera ewentualny kod błędu strony
 def zwracany_kod(x):
     if x == 404:
         return "Nie ma żadnych restauracji wspieranych przez Zomato  obok wskazanego miejsca"
@@ -24,7 +24,7 @@ def funkcja_szer_dlug(szerokosc, dlugosc):
     kod = r.status_code
     try:
         dane = r.json()
-
+    #przypisanie do listy potrzebmych dla nas danych
         for x in dane['nearby_restaurants']:
             listaAdresow.append((x['restaurant']['location']['latitude']))
             listaAdresow.append((x['restaurant']['location']['longitude']))
@@ -36,12 +36,22 @@ def funkcja_szer_dlug(szerokosc, dlugosc):
     return listaAdresow
 
 def funkcja_miasto_na_miasto_id(miasto):
+    zbior =[]
     headers = {'Accept': 'application/json', 'user-key': Key}
     r = requests.get(url + 'locations?query='+ str(miasto), headers=headers)
-    # kod który zwraca request
     kod = r.status_code
     dane = r.json()
     szerokosc = (dane['location_suggestions'][0]['latitude'])
     dlugosc =(dane['location_suggestions'][0]['longitude'])
-    return szerokosc,dlugosc
+    szerokosc1=szerokosc
+    dlugosc1  = dlugosc
+    #rozszerzenie wyszukania restauracji
+    for x in range(5):
+        zbior.append(szerokosc)
+        zbior.append(dlugosc)
+        szerokosc+=0.01
+        dlugosc += 0.01
+        szerokosc1 -=0.01
+        dlugosc1 -= 0.01
+    return zbior
 
