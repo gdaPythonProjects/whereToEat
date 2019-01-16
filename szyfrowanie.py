a36c48500a82b23cd96912e8367b00199c0327c1
GetTuhder <ktosspl@gmail.com>
2019-01-16 18:03:49 +0100

    asd

--- szyfrowanie.py
+++ szyfrowanie.py
@@ -1,12 +1,15 @@
 from cryptography.fernet import Fernet
+klucz=open('klucz.txt','r').read()
 key = Fernet.generate_key()
 # Put this somewhere safe!
 def szyfrowanie():
     f = Fernet(key)
-    token = f.encrypt(b"costam cosdtam")
+    token = f.encrypt(klucz)
     print(token)
 
 def deszyfrowanie():
     odkodowane = f.decrypt(token)
     return odkodowane
 'A really secret message. Not for prying eyes.'
+
+szyfrowanie();
